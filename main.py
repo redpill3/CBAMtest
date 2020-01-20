e53f56a5791fef098b26d78683fdb69c95d8c045
redpill3 <eldkrpdla121@naver.com>
2020-01-20 20:13:51 +0900

    Update main.py

--- main.py
+++ main.py
@@ -65,9 +65,44 @@ model = resnet_v1.resnet_v1(input_shape=(448,448,3), depth=depth, attention_modu
 # model = inception_resnet_v2.InceptionResNetV2(input_shape=input_shape, classes=num_classes, attention_module=attention_module)
 # model = densenet.DenseNet(input_shape=input_shape, classes=num_classes, attention_module=attention_module)
 
+'''
 model.compile(loss='categorical_crossentropy',
               optimizer=Adam(lr=lr_schedule(0)),
               metrics=['accuracy'])
+'''
+
+
+def weighted_categorical_crossentropy(weights):
+    """
+    A weighted version of keras.objectives.categorical_crossentropy
+
+    Variables:
+        weights: numpy array of shape (C,) where C is the number of classes
+
+    Usage:
+        weights = np.array([0.5,2,10]) # Class one at 0.5, class 2 twice the normal weights, class 3 10x.
+        loss = weighted_categorical_crossentropy(weights)
+        model.compile(loss=loss,optimizer='adam')
+    """
+
+    weights = K.variable(weights)
+
+    def loss(y_true, y_pred):
+        # scale predictions so that the class probas of each sample sum to 1
+        y_pred /= K.sum(y_pred, axis=-1, keepdims=True)
+        # clip to prevent NaN's and Inf's
+        y_pred = K.clip(y_pred, K.epsilon(), 1 - K.epsilon())
+        # calc
+        loss = y_true * K.log(y_pred) * weights
+        loss = -K.sum(loss, -1)
+        return loss
+
+    return loss
+
+model.compile(loss=weighted_categorical_crossentropy([4.375, 2.783, 1.301, 12.440, 1.285, 0.213, 10.075]),
+              optimizer=Adam(lr=lr_schedule(0)),
+              metrics=['accuracy'])
+
 model.summary()
 print(model_type)
 
